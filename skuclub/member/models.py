86005e76d3a74fd344f61851235d79c54a19e713
MeongW <easymiracle@skuniv.ac.kr>
2023-02-20 23:54:00 +0900

    Update models.py

--- skuclub/member/models.py
+++ skuclub/member/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 class Account(models.Model):
-    account_email = models.CharField(max_length=64, unique=True)
-    account_pw = models.CharField(max_length=32)
+    account_email = models.EmailField(unique=True)
+    account_pw = models.CharField(max_length=128)
     account_name = models.CharField(max_length=32)
     account_create_dt = models.DateTimeField(auto_now_add=True)
     
